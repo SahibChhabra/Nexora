103b6d5b8d6e6078c1c733d2b0830d89b491e2c4
Saheb Chhabra <sahibchhabraprog@gmail.com>
2026-09-04 23:13:00 +0530

    add analytics component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Teams from './components/Teams'
 import ContactsUs from './components/ContactsUs'
 import {Toaster} from 'react-hot-toast'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
    
@@ -15,6 +16,7 @@ const App = () => {
 
   return (
     <div className='dark:bg-black'>
+      <Analytics/>
       <Toaster/>
       <Navbar  theme={theme} settheme={settheme} />
       <Hero/>
